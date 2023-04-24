82fadafc170dfbfc1fb76a84b7d0cfdd89d643a9
adrian <adrian@localhost>
2023-04-24 20:44:34 +0200

    Make program usable for user via console.

--- src/Cipher.java
+++ src/Cipher.java
@@ -1,26 +1,26 @@
 public class Cipher {
     String[] alphabet = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q",
             "R","S","T","U","V","W","X","Y","Z"};
-    String[][] array = new String[5][5];
+    String[][] matrix = new String[5][5];
     String splittedLetter;
     String alphabetLetter;
     int l = 0;
     String lastReplacedLetter;
 
-    // method to send key word used to fill the array
+    // method to send key word used to fill the matrix
     void sendKeyWord(String keyWord) {
-        String[] splitted = keyWord.replaceAll("\\s", "").split("");
+        String[] splitted = keyWord.toUpperCase().replaceAll("\\s", "").split("");
         int k = 0;
 
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 if (k < splitted.length) {
                     splittedLetter = splitted[k];
                     boolean isDuplicate = false;
 
-                    for (int m = 0; m < array.length; m++) {
-                        for (int n = 0; n < array[m].length; n++) {
-                            String arrayLetter = array[m][n];
+                    for (int m = 0; m < matrix.length; m++) {
+                        for (int n = 0; n < matrix[m].length; n++) {
+                            String arrayLetter = matrix[m][n];
                             if (splittedLetter.equals("I")) {
                                 if (arrayLetter != null) {
                                     if (arrayLetter.equals("J")) {
@@ -42,13 +42,13 @@ public class Cipher {
                     }
 
                     if (!isDuplicate) {
-                        array[i][j] = splittedLetter;
+                        matrix[i][j] = splittedLetter;
                     } else {
                         j--;
                     }
                     k++;
                 } else {
-                    while (array[i][j] == null) {
+                    while (matrix[i][j] == null) {
                         alphabetLetter = alphabet[l];
                         boolean isDuplicate = false;
                         for (int m = 0; m < splitted.length; m++) {
@@ -68,7 +68,7 @@ public class Cipher {
                             }
                         }
                         if (!isDuplicate) {
-                            array[i][j] = alphabetLetter;
+                            matrix[i][j] = alphabetLetter;
                         }
                         l++;
                     }
@@ -78,9 +78,9 @@ public class Cipher {
     }
 
     void printArray() {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j] + " ");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
@@ -88,38 +88,38 @@ public class Cipher {
     }
 
     String encrypt(String word) {
-        String[] splitted = word.replaceAll("\\s", "").split("");
+        String[] splitted = word.toUpperCase().replaceAll("\\s", "").split("");
         StringBuilder sb = new StringBuilder();
 
         for (int k = 0; k < splitted.length; k+=2) {
             String letter1 = splitted[k];
             String letter2 = splitted[k+1];
-            System.out.print(letter1 + letter2 + " ");
+//            System.out.print(letter1 + letter2 + " ");
             int row1 = 0;
             int row2 = 0;
             int column1 = 0;
             int column2 = 0;
 
-            for (int i = 0; i < array.length; i++) {
-                for (int j = 0; j < array[i].length; j++) {
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
                     if (letter1.equals("I") || letter1.equals("J")) {
                         replace(letter1);
                     }
-                    if (letter1.equals(array[i][j])) {
+                    if (letter1.equals(matrix[i][j])) {
                         row1 = i;
                         column1 = j;
-//                        System.out.println(array[i][j]);
+//                        System.out.println(matrix[i][j]);
                     }
                     if (lastReplacedLetter != null) {
                         undoReplace();
                     }
 
                     if (letter2.equals("I") || letter2.equals("J")) {
-//                        System.out.print(array[1][0]);
+//                        System.out.print(matrix[1][0]);
                         replace(letter2);
-//                        System.out.print(array[1][0] + " ");
+//                        System.out.print(matrix[1][0] + " ");
                     }
-                    if (letter2.equals(array[i][j])) {
+                    if (letter2.equals(matrix[i][j])) {
                         row2 = i;
                         column2 = j;
                     }
@@ -130,77 +130,76 @@ public class Cipher {
             }
 
             if (row1 == row2) {
-                if (column1 != array[row1].length-1) {
-                    letter1 = array[row1][column1+1];
+                if (column1 != matrix[row1].length-1) {
+                    letter1 = matrix[row1][column1+1];
                 } else {
-                    letter1 = array[row1][0];
+                    letter1 = matrix[row1][0];
                 }
-                if (column2 != array[row2].length-1) {
-                    letter2 = array[row2][column2+1];
+                if (column2 != matrix[row2].length-1) {
+                    letter2 = matrix[row2][column2+1];
                 } else {
-                    letter2 = array[row2][0];
+                    letter2 = matrix[row2][0];
                 }
             } else if (column1 == column2) {
 //                System.out.println("row = " + row1 + " column = " + column2
-//                        + " letter = " + array[row1][column2]);
+//                        + " letter = " + matrix[row1][column2]);
 //                System.out.println("row = " + row2 + " column = " + column2
-//                        + " letter = " + array[row2][column2]);
-                if (row1 != array[column1].length-1) {
-                    letter1 = array[row1+1][column1];
+//                        + " letter = " + matrix[row2][column2]);
+                if (row1 != matrix[column1].length-1) {
+                    letter1 = matrix[row1+1][column1];
                 } else {
-                    letter1 = array[0][column1];
+                    letter1 = matrix[0][column1];
                 }
-                if (row2 != array[column2].length-1) {
-                    letter2 = array[row2+1][column2];
+                if (row2 != matrix[column2].length-1) {
+                    letter2 = matrix[row2+1][column2];
                 } else {
-                    letter2 = array[0][column2];
+                    letter2 = matrix[0][column2];
                 }
             } else {
-                letter1 = array[row1][column2];
-                letter2 = array[row2][column1];
+                letter1 = matrix[row1][column2];
+                letter2 = matrix[row2][column1];
             }
 
             sb.append(letter1).append(letter2);
             sb.append(" ");
         }
 
-        System.out.println();
         return sb.toString();
     }
 
     String decrypt(String word) {
-        String[] splitted = word.replaceAll("\\s", "").split("");
+        String[] splitted = word.toUpperCase().replaceAll("\\s", "").split("");
         StringBuilder sb = new StringBuilder();
 
         for (int k = 0; k < splitted.length; k+=2) {
             String letter1 = splitted[k];
             String letter2 = splitted[k+1];
-            System.out.print(letter1 + letter2 + " ");
+//            System.out.print(letter1 + letter2 + " ");
             int row1 = 0;
             int row2 = 0;
             int column1 = 0;
             int column2 = 0;
 
-            for (int i = 0; i < array.length; i++) {
-                for (int j = 0; j < array[i].length; j++) {
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
                     if (letter1.equals("I") || letter1.equals("J")) {
                         replace(letter1);
                     }
-                    if (letter1.equals(array[i][j])) {
+                    if (letter1.equals(matrix[i][j])) {
                         row1 = i;
                         column1 = j;
-//                        System.out.println(array[i][j]);
+//                        System.out.println(matrix[i][j]);
                     }
                     if (lastReplacedLetter != null) {
                         undoReplace();
                     }
 
                     if (letter2.equals("I") || letter2.equals("J")) {
-//                        System.out.print(array[1][0]);
+//                        System.out.print(matrix[1][0]);
                         replace(letter2);
-//                        System.out.print(array[1][0] + " ");
+//                        System.out.print(matrix[1][0] + " ");
                     }
-                    if (letter2.equals(array[i][j])) {
+                    if (letter2.equals(matrix[i][j])) {
                         row2 = i;
                         column2 = j;
                     }
@@ -212,56 +211,55 @@ public class Cipher {
 
             if (row1 == row2) {
                 if (column1 != 0) {
-                    letter1 = array[row1][column1-1];
+                    letter1 = matrix[row1][column1-1];
                 } else {
-                    letter1 = array[row1][array[row1].length-1];
+                    letter1 = matrix[row1][matrix[row1].length-1];
                 }
                 if (column2 != 0) {
-                    letter2 = array[row2][column2+1];
+                    letter2 = matrix[row2][column2+1];
                 } else {
-                    letter2 = array[row2][array[row2].length-1];
+                    letter2 = matrix[row2][matrix[row2].length-1];
                 }
             } else if (column1 == column2) {
 //                System.out.println("row = " + row1 + " column = " + column2
-//                        + " letter = " + array[row1][column2]);
+//                        + " letter = " + matrix[row1][column2]);
 //                System.out.println("row = " + row2 + " column = " + column2
-//                        + " letter = " + array[row2][column2]);
+//                        + " letter = " + matrix[row2][column2]);
                 if (row1 != 0) {
-                    letter1 = array[row1-1][column1];
+                    letter1 = matrix[row1-1][column1];
                 } else {
-                    letter1 = array[array.length-1][column1];
+                    letter1 = matrix[matrix.length-1][column1];
                 }
                 if (row2 != 0) {
-                    letter2 = array[row2-1][column2];
+                    letter2 = matrix[row2-1][column2];
                 } else {
-                    letter2 = array[array.length-1][column2];
+                    letter2 = matrix[matrix.length-1][column2];
                 }
             } else {
-                letter1 = array[row1][column2];
-                letter2 = array[row2][column1];
+                letter1 = matrix[row1][column2];
+                letter2 = matrix[row2][column1];
             }
 
             sb.append(letter1).append(letter2);
             sb.append(" ");
         }
 
-        System.out.println();
         return sb.toString();
     }
 
-    // method to replace "I" or "J" as they share the same array index
+    // method to replace "I" or "J" as they share the same matrix index
     void replace(String letter) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 if (letter.equals("I")) {
-                    if (array[i][j].equals("J")) {
-                        array[i][j] = "I";
+                    if (matrix[i][j].equals("J")) {
+                        matrix[i][j] = "I";
                         lastReplacedLetter = "J";
                         break;
                     }
                 } else if (letter.equals("J")) {
-                    if (array[i][j].equals("I")) {
-                        array[i][j] = "J";
+                    if (matrix[i][j].equals("I")) {
+                        matrix[i][j] = "J";
                         lastReplacedLetter = "I";
                         break;
                     }
@@ -274,15 +272,15 @@ public class Cipher {
 
     // method to undo replacing
     void undoReplace() {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                if (array[i][j].equals("I")) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j].equals("I")) {
                     if (lastReplacedLetter.equals("J")) {
-                        array[i][j] = "J";
+                        matrix[i][j] = "J";
                     }
-                } else if (array[i][j].equals("J")) {
+                } else if (matrix[i][j].equals("J")) {
                     if (lastReplacedLetter.equals("I")) {
-                        array[i][j] = "I";
+                        matrix[i][j] = "I";
                     }
                 }
             }
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Cipher cipher = new Cipher();
 
-        cipher.sendKeyWord("PLAYFAIR EXAMPLE");
+        System.out.print("Key word = ");
+        String keyWord = sc.nextLine();
+        cipher.sendKeyWord(keyWord);
         System.out.println();
-        System.out.println("Array made of sent key word");
+        System.out.println("Matrix made of given key word");
         cipher.printArray();
 
-        System.out.println("Encrypting");
-        String encrypted = cipher.encrypt("TEKST JAWNY");
-        System.out.println(encrypted);
+        System.out.print("Text to encrypt = ");
+        String text = sc.nextLine();
+        String encrypted = cipher.encrypt(text);
+        System.out.print("Encrypted = " + encrypted);
 
         System.out.println();
-        System.out.println("Decrypting");
-        String decrypted = cipher.decrypt("VI NK PB YV QL");
-        System.out.println(decrypted);
+        System.out.print("Text to decrypt = ");
+        text = sc.nextLine();
+        String decrypted = cipher.decrypt(text);
+        System.out.print("Decrypted = " + decrypted);
     }
 }
